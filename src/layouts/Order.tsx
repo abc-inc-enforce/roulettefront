235704eb28bfa2e5aef31239810860c3f3f1c300
Yujeong12 <yujeong8974@gmail.com>
2023-11-14 10:47:28 +0900

    UPDATE : PriceUpgrade page, Order page 카테고리 선택시 표시 메뉴 변경

--- src/layouts/Order.tsx
+++ src/layouts/Order.tsx
@@ -3,18 +3,28 @@ import Food from "../components/Food";
 import category from "../data/category";
 import menu from "../data/menu";
 import sample from "../assets/sample.png";
+import { useState } from "react";
 
 const Order = () => {
+  const [categoryValue, setcategoryValue] = useState("메인");
   return (
     <S.body>
       <S.sidebar>
         <S.tableNum>1번 테이블</S.tableNum>
         <S.categoryList>
           {category.map(({ name }, index) =>
-            name === "메인" ? (
-              <S.category color="white">{name}</S.category>
+            name === categoryValue ? (
+              <S.category
+                onClick={() => setcategoryValue(name)}
+                key={index}
+                color="white"
+              >
+                {name}
+              </S.category>
             ) : (
-              <S.category>{name}</S.category>
+              <S.category onClick={() => setcategoryValue(name)} key={index}>
+                {name}
+              </S.category>
             ),
           )}
         </S.categoryList>
@@ -28,12 +38,17 @@ const Order = () => {
         </S.header>
         <S.menu>
           <ul>
-            {menu.map(({ img, name, price }, index) => (
-              <li>
-                <Food foodImg={img} foodName={name} foodPrice={price + "원"} />
-              </li>
-            ))}
-            {/* <Food foodImg={sample} foodName="치킨" foodPrice="2000원" /> */}
+            {menu.map(({ img, name, category, price }, index) =>
+              category === categoryValue ? (
+                <li key={index}>
+                  <Food
+                    foodImg={img}
+                    foodName={name}
+                    foodPrice={price + "원"}
+                  />
+                </li>
+              ) : null,
+            )}
           </ul>
         </S.menu>
         <S.orderBtn>주문</S.orderBtn>
